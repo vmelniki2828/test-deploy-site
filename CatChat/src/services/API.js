82a845c6c96562040bd6e8fc5a42e40630a36af7
Vlad Melnik <vmelniki2828@gmail.com>
2024-08-25 05:21:48 +0300

    try 100000000000000000

--- CatChat/src/services/API.js
+++ CatChat/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3001', {
+export const socket = io('http://95.164.33.221', {
   transports: ['websocket', 'polling'],
   withCredentials: true,
   reconnection: true, // Включает автоматическое восстановление соединения
