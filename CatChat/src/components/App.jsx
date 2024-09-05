e49454e30e9da661dfed30dbdb12d207fac5f1b9
Vlad Melnik <vmelniki2828@gmail.com>
2024-09-05 16:17:16 +0300

    try

--- CatChat/src/components/App.jsx
+++ CatChat/src/components/App.jsx
@@ -32,7 +32,7 @@ export const App = () => {
   const handleSearch = async () => {
     try {
       const response = await axios.get(
-        `http://95.164.33.221/api/rooms/${uname}`
+        `http://95.164.33.221:8000/api/rooms/${uname}`
       );
       setChats(response.data);
     } catch (error) {
