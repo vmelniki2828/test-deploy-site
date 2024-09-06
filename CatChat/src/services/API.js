9f131a8749d566889a19faac4d70c4ee3ea32063
Vlad Melnik <vmelniki2828@gmail.com>
2024-09-06 11:08:23 +0300

    asd

--- CatChat/src/services/API.js
+++ CatChat/src/services/API.js
@@ -9,63 +9,67 @@ export const socket = io('http://95.164.33.221:8000', {
   reconnectionDelay: 1000, // Задержка между попытками восстановления (в миллисекундах)
 });
 
-export const getArchivedRooms = (uname) => {
-  socket.emit("get_archived_rooms", { username: uname });
+export const getArchivedRooms = uname => {
+  socket.emit('get_archived_rooms', { username: uname });
 };
-
 
 axios.defaults.baseURL = 'http://95.164.33.221:8000/api';
 
-  
-  export const token = {
-    set(token) {
-      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    },
-    unset() {
-      axios.defaults.headers.common.Authorization = '';
-    },
-  };
-  
-  export const loginFoo = async credentials => {
-    const { data } = await axios.post('token/login', credentials);
-  
-    return data;
-  };
-  export const getCurrentUser = async credentials => {
-    const { data } = await axios.get('user', credentials);
-    return data;
-  };
-  
-  export const getCurrentUserToken = async token => {
-    try {
-      const { data } = await axios.get('user', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return data;
-    } catch (error) {
-      console.error(
-        'Error fetching data:',
-        error.response?.data || error.message
-      );
-      throw error;
-    }
-  };
+export const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
 
-  export const getUserProfile = async token => {
-    try {
-      const result = await axios.get('user/profile', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return result.data;
-    } catch (error) {
-      console.error(
-        'Error fetching data:',
-        error.response?.data || error.message
-      );
-      throw error;
-    }
-  };
+export const loginFoo = async credentials => {
+  const { data } = await axios.post('token/login', credentials);
+
+  return data;
+};
+export const getCurrentUser = async credentials => {
+  const { data } = await axios.get('user', {
+    headers: {
+      Authorization: `Bearer ${credentials}`, // Передаем токен
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true, // Для передачи куки и авторизационных данных
+  });
+  return data;
+};
+
+export const getCurrentUserToken = async token => {
+  try {
+    const { data } = await axios.get('user', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error(
+      'Error fetching data:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
+
+export const getUserProfile = async token => {
+  try {
+    const result = await axios.get('user/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return result.data;
+  } catch (error) {
+    console.error(
+      'Error fetching data:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
