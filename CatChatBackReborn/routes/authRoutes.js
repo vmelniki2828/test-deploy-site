87c30e75de7d47951f46d46b66977bf2ceee6e31
Vlad Melnik <vmelniki2828@gmail.com>
2024-09-06 11:15:01 +0300

    asd

--- CatChatBackReborn/routes/authRoutes.js
+++ CatChatBackReborn/routes/authRoutes.js
@@ -1,56 +1,43 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 const router = express.Router();
 
 const app = express();
 
 // Настройка CORS
-app.use(
-  cors({
-    origin: "http://95.164.33.221:3000", // Укажите здесь ваш фронтенд домен
-    methods: ["GET", "POST", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Установите true, если ваш фронтенд использует куки
-  })
-);
+app.use(cors({
+  origin: 'http://95.164.33.221:3000', // Укажите здесь ваш фронтенд домен
+  methods: ["GET", "POST", "PUT"],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Установите true, если ваш фронтенд использует куки
+}));
 // Установите базовый URL для запросов
 axios.defaults.baseURL = `https://test.cat-tools.com/api`;
 
 app.use(express.json());
 
 // Маршрут для авторизации (логина)
-router.post("/token/login", async (req, res) => {
+router.post('/token/login', async (req, res) => {
   try {
-    const { data } = await axios.post("token/login", req.body);
+    const { data } = await axios.post('token/login', req.body);
     res.json(data);
-    console.log(data);
   } catch (error) {
-    console.error(
-      "Ошибка при авторизации:",
-      error.response?.data || error.message
-    );
-    res.status(500).json({ error: "Ошибка при авторизации" });
+    console.error('Ошибка при авторизации:', error.response?.data || error.message);
+    res.status(500).json({ error: 'Ошибка при авторизации' });
   }
 });
 
-router.get("/user", async (req, res) => {
+router.get('/user', async (req, res) => {
   try {
-    const { data } = await axios.get("user", {
-      headers: {
-        Authorization: `Bearer ${req.body}`, // Передаем токен
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
-    });
+    console.log(req.body)
+    const { data } = await axios.get('user', req.body);
     res.json(data);
   } catch (error) {
-    console.error(
-      "Ошибка при авторизации:",
-      error.response?.data || error.message
-    );
-    res.status(500).json({ error: "Ошибка при авторизации" });
+    console.error('Ошибка при авторизации:', error.response?.data || error.message);
+    res.status(500).json({ error: 'Ошибка при авторизации' });
   }
 });
+
 
 module.exports = router;
